495416371a3a1d1c1d181568d58b8c7b7a852958
Riccardo Crippa <therickys93@gmail.com>
2017-06-28 16:33:24 +0200

    eliminazione tutti prodotti funziona

--- app/src/main/java/it/therickys93/barorder/ProductsListActivity.java
+++ app/src/main/java/it/therickys93/barorder/ProductsListActivity.java
@@ -3,71 +3,41 @@ package it.therickys93.barorder;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import it.therickys93.javabarorderapi.BarOrder;
-import it.therickys93.javabarorderapi.CompleteOrder;
-import it.therickys93.javabarorderapi.Order;
-import it.therickys93.javabarorderapi.Orders;
+import it.therickys93.javabarorderapi.DeleteProductAll;
+import it.therickys93.javabarorderapi.Product;
+import it.therickys93.javabarorderapi.Products;
 import it.therickys93.javabarorderapi.Response;
 
-public class OrdersActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class ProductsListActivity extends AppCompatActivity {
 
     private ListView listView;
-    private List<Order> orders;
-    private OrderAdapter adapter;
-    private static int TIMER_DELAY = 1000;
-    private Handler handler;
+    private List<Product> prodotti;
+    private ProductAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_orders);
+        setContentView(R.layout.activity_products_list);
 
-        handler = new Handler();
-        startRepeatingTask();
-
-        this.listView = (ListView) findViewById(R.id.ordersListView);
-        new BarOrderGetOrders().execute();
-        this.listView.setOnItemClickListener(this);
+        this.listView = (ListView) findViewById(R.id.productsList);
+        new BarOrderGetProducts().execute();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        stopRepeatingTask();
-    }
-
-    Runnable mStatusChecker = new Runnable() {
-        @Override
-        public void run() {
-            try {
-                new BarOrderGetOrders().execute();
-            } finally {
-                handler.postDelayed(mStatusChecker, TIMER_DELAY);
-            }
-        }
-    };
-
-    void startRepeatingTask() {
-        mStatusChecker.run();
-    }
-
-    void stopRepeatingTask() {
-        handler.removeCallbacks(mStatusChecker);
+    public void deleteProductsAll(View view) {
+        new BarOrderDeleteProductsAll().execute();
+        new BarOrderGetProducts().execute();
     }
 
     @Override
@@ -114,52 +84,47 @@ public class OrdersActivity extends AppCompatActivity implements AdapterView.OnI
         }
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int index, long l) {
-        Order order = this.orders.get(index);
-        new BarOrderCompleteOrder().execute(order);
-        new BarOrderGetOrders().execute();
-    }
-
-    private class BarOrderGetOrders extends AsyncTask<Void, Void, List<Order>> {
+    private class BarOrderGetProducts extends AsyncTask<Void, Void, List<Product>> {
 
         @Override
-        protected List<Order> doInBackground(Void... voids) {
+        protected List<Product> doInBackground(Void... voids) {
             SharedPreferences settings = getSharedPreferences("MySettingsBarOrder", 0);
             String url = settings.getString("BARORDER_URL", "192.168.1.10");
             BarOrder barorder = new BarOrder("http://" + url);
             try {
-                String response = barorder.execute(new Orders());
-                List<Order> ordini = Response.parseOrders(response);
-                return ordini;
+                String response = barorder.execute(new Products());
+                List<Product> products = Response.parseProducts(response);
+                return products;
             } catch (Exception e) {
+                e.printStackTrace();
                 System.out.println("Exception: " + e.getMessage());
                 return null;
             }
         }
 
         @Override
-        protected void onPostExecute(List<Order> ordini) {
-            super.onPostExecute(ordini);
-            orders = ordini;
-            adapter = new OrderAdapter(OrdersActivity.this, orders);
+        protected void onPostExecute(List<Product> products) {
+            super.onPostExecute(products);
+            prodotti = products;
+            adapter = new ProductAdapter(ProductsListActivity.this, prodotti);
             listView.setAdapter(adapter);
         }
     }
 
-    private class BarOrderCompleteOrder extends AsyncTask<Order, Void, Boolean> {
+    private class BarOrderDeleteProductsAll extends AsyncTask<Void, Void, Boolean> {
 
         @Override
-        protected Boolean doInBackground(Order... orders) {
+        protected Boolean doInBackground(Void... voids) {
             SharedPreferences settings = getSharedPreferences("MySettingsBarOrder", 0);
             String url = settings.getString("BARORDER_URL", "192.168.1.10");
             BarOrder barorder = new BarOrder("http://" + url);
             try {
-                String response = barorder.execute(new CompleteOrder(orders[0]));
-                Response status = Response.parseSuccess(response);
-                return status.ok();
-            } catch (IOException e) {
+                String response = barorder.execute(new DeleteProductAll());
+                Response responseObj = Response.parseSuccess(response);
+                return responseObj.ok();
+            } catch (Exception e) {
                 e.printStackTrace();
+                System.out.println("Exception: " + e.getMessage());
                 return false;
             }
         }
@@ -168,9 +133,9 @@ public class OrdersActivity extends AppCompatActivity implements AdapterView.OnI
         protected void onPostExecute(Boolean aBoolean) {
             super.onPostExecute(aBoolean);
             if(aBoolean) {
-                Toast.makeText(OrdersActivity.this, "Ordine completato con successo!!!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ProductsListActivity.this, "Prodotti eliminati con successo!!!", Toast.LENGTH_SHORT).show();
             } else {
-                Toast.makeText(OrdersActivity.this, "Riprova!! Ordine non completato!!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ProductsListActivity.this, "Riprova!! Prodotti non eliminati!!", Toast.LENGTH_SHORT).show();
             }
         }
     }
