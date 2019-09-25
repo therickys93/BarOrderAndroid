f1085a53d381c6480b3b28c51ca3ca185b1943ab
Riccardo Crippa <therickys93@gmail.com>
2019-09-25 20:29:07 +0200

    aggiunto campo token nelle impostazioni

--- app/src/main/java/it/therickys93/barorder/SettingsActivity.java
+++ app/src/main/java/it/therickys93/barorder/SettingsActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class SettingsActivity extends AppCompatActivity {
 
     private EditText editTextUrl;
+    private EditText editTextToken;
     private EditText editTextTableMin;
     private EditText editTextTableMax;
     private TextView appVersionTextView;
@@ -20,6 +21,7 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         this.editTextUrl = (EditText) findViewById(R.id.editTextUrl);
+        this.editTextToken = (EditText) findViewById(R.id.editTextToken);
         this.editTextTableMin = (EditText) findViewById(R.id.editTextTableMin);
         this.editTextTableMax = (EditText) findViewById(R.id.editTextTableMax);
         this.appVersionTextView = (TextView) findViewById(R.id.appVersion);
@@ -28,18 +30,21 @@ public class SettingsActivity extends AppCompatActivity {
 
         SharedPreferences settings = getSharedPreferences("MySettingsBarOrder", 0);
         String url = settings.getString("BARORDER_URL", "http://192.168.1.10");
+        String token = settings.getString("BARORDER_TOKEN", "password");
         String tableMax = settings.getString("BARORDER_TABLE_MAX", "20");
         String tableMin = settings.getString("BARORDER_TABLE_MIN", "0");
 
         this.editTextUrl.setText(url);
         this.editTextTableMin.setText(tableMin);
         this.editTextTableMax.setText(tableMax);
+        this.editTextToken.setText(token);
     }
 
     public void doneSettings(View view) {
         SharedPreferences settings = getSharedPreferences("MySettingsBarOrder", 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putString("BARORDER_URL", this.editTextUrl.getText().toString());
+        editor.putString("BARORDER_TOKEN", this.editTextToken.getText().toString());
         editor.putString("BARORDER_TABLE_MIN", this.editTextTableMin.getText().toString());
         editor.putString("BARORDER_TABLE_MAX", this.editTextTableMax.getText().toString());
         editor.commit();
